844879221b7d2670bd3dd5c6e015a5643d4f5a24
Ahmed Jawad <ahmed.jawad32123@gmail.com>
2025-10-06 22:15:02 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -276,3 +276,27 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
 });
+
+
+
+//==================== FAQ collapse/expand====================================
+document.addEventListener('DOMContentLoaded', () => {
+  const faqQuestions = document.querySelectorAll('.faq-question');
+
+  faqQuestions.forEach(question => {
+    question.addEventListener('click', () => {
+      const answer = question.nextElementSibling;
+      answer.classList.toggle('open');
+    });
+  });
+});
+
+
+
+
+
+
+
+
+
+
